5d1e226b718394a71b062227de664ad12b4d9084
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-04-21 16:31:06 +0530

    graph plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
+
 plt.plot(squares, linewidth=3) #line linewidth controls the thickness of the line
 #set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
