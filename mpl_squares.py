eaabc6fb5f79572ea40521e78fae118ef0b36938
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-04-22 11:27:04 +0530

    scatter points

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-plt.plot(squares, linewidth=2) #line linewidth controls the thickness of the line
+plt.plot(input_values, squares, linewidth=2) #line linewidth controls the thickness of the line
 #set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
---
+++ scatter_squares.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.scatter(2, 4, s=200)
+#set chart title and label axes
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Values", fontsize=14)
+plt.ylabel("Square of values", fontsize=14)
+#set size of a tick labels
+
+plt.show()
