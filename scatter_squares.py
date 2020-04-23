f129e0896bb26a4a52c198778d74c5983dfe9f40
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-04-23 10:48:44 +0530

    modified scatter

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
-
-plt.scatter(2, 4, s=200)
+# scatter means individual points, we can customize that with colors and size or anything, we can customize every single point in the plot
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+plt.scatter(x_values, y_values, s=50)
 #set chart title and label axes
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Values", fontsize=14)
