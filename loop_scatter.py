e3c27bb8a447386f4a4276c8fbd32d1d2c38ec3c
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-04-25 11:04:56 +0530

    color modules

--- loop_scatter.py
+++ loop_scatter.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values] #here we square a number and store it in y_values
 
-plt.scatter(x_values, y_values, s = 5) #we pass all the x and y values
+plt.scatter(x_values, y_values, c='cyan', edgecolor='none', s = 5) #we pass all the x and y values
+# edgecolor used to solid points and c is a color we can use
 plt.title("Square Numbers", fontsize=22)
 plt.xlabel("Values", fontsize=14)
 plt.ylabel("Square of values", fontsize=14)
