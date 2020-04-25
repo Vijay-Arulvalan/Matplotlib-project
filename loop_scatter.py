dde1a0d9a355f3f34d791f85af22808b51184fd8
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-04-25 11:21:37 +0530

    colormap module

--- loop_scatter.py
+++ loop_scatter.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values] #here we square a number and store it in y_values
 
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s = 5) #we pass all the x and y values
-# edgecolor used to solid points and c is a color we can use #to define color in RGB color module 3 decimal points one for red and one for blue and one for green
+plt.scatter(x_values, y_values, c=(0, 0.5, 0.5), edgecolor='none', s = 5) #we pass all the x and y values
+# edgecolor used to solid points and c is a color we can use #to define color in RGB color module 3 decimal points one for red and one for green and one for blue
 #using values between 0 and 1, values closer to 0 produce dark color and values closer to 1 produce light colors
 plt.title("Square Numbers", fontsize=22)
 plt.xlabel("Values", fontsize=14)
