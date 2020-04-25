ad4ba7706714222f212448e60621249b33cd2484
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-04-25 11:00:02 +0530

    scatter modified

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 # scatter means individual points, we can customize that with colors and size or anything, we can customize every single point in the plot
 x_values = [1, 2, 3, 4, 5]
 y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, edgecolor='none', s=40)
+plt.scatter(x_values, y_values, edgecolor='none', s=40) #edgecolor for only solid blue points
 #set chart title and label axes
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Values", fontsize=14)
